d7f69be75eb74f6b1449b8d54637ae590bcaf6cd
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-12-10 13:55:26 -0300

    fim da aula 01

--- internal/market/entity/order.go
+++ internal/market/entity/order.go
@@ -1,27 +1,27 @@
 package entity
 
 type Order struct {
-  ID string
-  Investor *Investor
-  Asset *Asset
-  Shares int
-  PendingShares int
-  Price float64
-  OrderType string
-  Status string
-  Transactions []*Transaction
+	ID            string
+	Investor      *Investor
+	Asset         *Asset
+	Shares        int
+	PendingShares int
+	Price         float64
+	OrderType     string
+	Status        string
+	Transactions  []*Transaction
 }
 
 func NewOrder(orderID string, pendingShares int, investor *Investor, asset *Asset, shares int, price float64, orderType string) *Order {
-  return &Order{
-    ID: orderID,
-    Investor: investor,
-    Asset: asset,
-    PendingShares: pendingShares,
-    Shares: shares,
-    Price: price,
-    OrderType: orderType,
-    Status: "OPEN",
-    Transactions: []*Transaction{},
-  }
+	return &Order{
+		ID:            orderID,
+		Investor:      investor,
+		Asset:         asset,
+		PendingShares: pendingShares,
+		Shares:        shares,
+		Price:         price,
+		OrderType:     orderType,
+		Status:        "OPEN",
+		Transactions:  []*Transaction{},
+	}
 }
